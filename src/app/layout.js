b4f34e0ce1ecbbeabe25a34082cd92451d9ec673
Len Wright <lensreacr@gmail.com>
2024-07-01 08:42:30 +0100

    second commit of the app

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,12 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Next Movie App",
-  description: "Nextjs Movie Tutorial App",
+  description: "Nextjs IMDB Tutorial App",
 };
 
 export default function RootLayout({ children }) {
